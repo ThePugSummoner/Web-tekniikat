e8ac963adf4a1fd8fa92058f48324bc2888cbc71
Ilia Zubov <97738404+ThePugSummoner@users.noreply.github.com>
2022-05-22 12:30:55 +0300

    page, imagex, sass, json added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 This is readme-file for Web-teknikaat repository
+
+Tämä proekti on keskellä
+
+index.html sivu tehty bootstrapin template pohjalla ja css tyyli määrittelty erikseen (värit, fontit jne.)
+
+images kansio sisältää kuvat pixabay.com:sta
+
+booking.html sivulla oleva varaus kalenteri toteutettu jquery avulla
+
+maasto.html sivu toteutettu onload() funktiolla joka muutta otsikon h3 sekä button elementin nimet (sisältö ei muuttu(työn alla))
+
+kaupunki.html sisältä json taulukko joissa on varastettujen pyörien tietoa
+
--- js/script.js
+++ js/script.js
@@ -21,4 +21,93 @@ $( function() {
         form.classList.add('was-validated')
       }, false)
     })
-})()
+})();
+
+//Advanced js
+
+let bikeModel = [
+  
+  {
+    type: "Kaupunkipyörät",
+    image: "../images/bicycle-780049_640.jpg"
+  },
+
+  {
+    type: "Lasten polkupyörät"
+  },
+
+  {
+    type: "Hybridipyörät"
+  },
+
+  {
+    type: "Sähköpyörät"
+  },
+
+  {
+    type: "Maastopyörät",
+    discipline: [
+      "Cross Country",
+      "Trail",
+      "Enduro",
+      "Downhill",
+      "Dirt/Street",
+      "Fat bike"
+    ]
+  },
+
+  {
+    type: "Maantiepyörät"
+  }
+
+];
+
+let h3Element = document.querySelector("h3");
+let buttonElement1 = document.querySelector("#flush-headingOne .accordion-button");
+let buttonElement2 = document.querySelector("#flush-headingTwo .accordion-button");
+let buttonElement3 = document.querySelector("#flush-headingThree .accordion-button");
+let buttonElement4 = document.querySelector("#flush-headingFour .accordion-button");
+let pictureElement = document.querySelector(".accordion");
+
+function bikeType1 () {
+  h3Element.innerHTML = bikeModel[0].type;
+  pictureElement.innerHTML = "<img id ='image1' src ='"+ bikeModel[0].image +"' class='rounded mx-auto d-block my-5' alt=''/>"
+}
+
+
+function bikeType5 () {
+  h3Element.innerHTML = bikeModel[4].type;
+  buttonElement1.innerHTML = bikeModel[4].discipline[0];
+  buttonElement2.innerHTML = bikeModel[4].discipline[1];
+  buttonElement3.innerHTML = bikeModel[4].discipline[2];
+  buttonElement4.innerHTML = bikeModel[4].discipline[3];
+}
+
+
+fetch('https://bikeindex.org/api/v3/search')
+
+.then(response => response.json())
+
+.then(data => {
+
+  let ul = document.createElement('ul');
+
+  data.bikes.forEach (title => {
+
+    let li = document.createElement('li');
+
+    li.innerHTML = title.title + " : " + title.status;
+
+    ul.appendChild(li);
+  })
+
+  document.getElementById('print-here').appendChild(ul);
+  
+});
+
+let h5 = document.createElement('h5');
+
+  h5.innerHTML = "Stolen bikes database";
+
+  document.getElementById('print-here').appendChild(h5);
+
